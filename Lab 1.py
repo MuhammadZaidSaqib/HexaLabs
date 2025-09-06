3910fd0c106b65c3720a863f1b6648bcac345df3
Muhammad Zaid Saqib <B24F1722CYS084@paf-iast.edu.pk>
2025-09-06 22:16:25 +0500

    Updated Lab 1.py

---
+++ Lab 1.py
@@ -0,0 +1,32 @@
+#Name: Muhammad Zaid Saqib
+#Reg No: B24F1722CYS084
+#Introducation to AI
+#Lab 1
+
+
+# print
+#print(" boy you're hacked enjoy ")
+
+
+# sum
+
+'''a=float(input(" Enter the first number"))
+b=float(input(" Enter the second number"))
+c=a+b
+print("The sum of C ", c)'''
+
+#solve education with more numbers
+'''a=float(input(" Enter the first number"))
+b=float(input(" Enter the second number"))
+c=float(input(" Enter the third number"))
+d=float(a/b*c)
+print("The sum of d ", d)'''
+
+# print string
+'''user_name=input("Enter your username")
+print (" Mr ",user_name ," you are cooked which mean i hacked you")'''
+
+
+'''name=input("Enter your name")
+age=input("Enter your age")
+print("My name is ",name,"and your age is ",age)'''
